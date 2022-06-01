65b2d21cc8a0536231021de47bdb8da72e1a2668
hattajr <atadonjep@gmail.com>
2022-06-01 12:20:36 +0900

    [MNT] push env

--- examples/test_speed.py
+++ examples/test_speed.py
@@ -2,5 +2,8 @@
 # froi
 # from scipy import signal
 from backend import gotstools
+import pandas as pd
 
 
+data = pd.read_csv('/home/hattajr/workdir/datasets/2021201_2021205_HisItemcurrsecond.csv')
+print(data.head())
